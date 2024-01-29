29b7c5704bf0d4afcfee7d71ae488a5f31278cd5
최재성 <we0620@naver.com>
2024-01-29 22:20:35 +0900

    출발,도착위도,경도파악완료,지도필요

--- frontend/src/pages/Service/Cleaning/RequestCleaning.jsx
+++ frontend/src/pages/Service/Cleaning/RequestCleaning.jsx
@@ -1,14 +1,10 @@
 import Chat from "../../../components/Chat/Chat";
 import Header from "../../../components/Header";
-import Map from "../../../components/Service/Delivery/Map";
 
 
 export default function RequestCleaning() {
     return <>
         <Header />
         <Chat />
-        <div style={{marginTop:"10rem"}}>
-        <Map/>
-        </div>
         </>
 }
