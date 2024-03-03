07589b39a8f2bf68a3b345816c3cdc0263126b47
최재성 <we0620@naver.com>
2024-03-03 23:14:27 +0900

    react_native

---
+++ react_native/my_app/screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+
+function RecentExpenses() {
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+}
+export default RecentExpenses;
